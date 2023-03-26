6227ab1b1db4d60b473f752fefb77bc4e3b2abf4
chandra575 <kmalware88@gmail.com>
2023-03-26 20:20:09 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_1302210134;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    
+
+        string videoTitle = "Tutorial Design By Contract - Chandra Bayu Samudra";
+        SayaTubeVideo myVideo = new SayaTubeVideo(videoTitle);
+        myVideo.IncreasePlayCount(1000);
+        for (int i = 0; i < 20000000; i += 10000000)
+        {
+            try
+            {
+                myVideo.IncreasePlayCount(10000000);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                break;
+            }
+        }
+        myVideo.PrintVideoDetails();
+
+        Console.ReadLine(); 
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302210134
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string videoTitle)
+        {
+            
+            if (videoTitle == null || videoTitle.Length > 100)
+            {
+                throw new ArgumentException("Judul video harus berisi antara 1-100 karakter");
+            }
+
+            Random rand = new Random();
+            id = rand.Next(10000, 99999);
+            title = videoTitle;
+            playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count < 0 || count > 10000000)
+            {
+                throw new ArgumentOutOfRangeException("Jumlah penambahan play count harus di antara 0-10.000.000");
+            }
+
+            try
+            {
+                playCount = checked(playCount + count);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("================================= Video Details =================================");
+            Console.WriteLine("ID           : " + id);
+            Console.WriteLine("Judul        : " + title);
+            Console.WriteLine("Play Count   : " + playCount);
+            Console.WriteLine("=================================================================================");
+        }
+    }
+}
